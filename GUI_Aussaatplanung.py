7df8ff4cd2289e1606a3f2d3a900d498aa6e40dc
Justin Fuchs <j.fuchs@tu-braunschweig.de>
2023-12-14 04:40:12 +0100

    GUI

--- GUI_Aussaatplanung.py
+++ GUI_Aussaatplanung.py
@@ -7,7 +7,8 @@ from rotate_pattern import generate_triangular_points_rotated
 from shapely.geometry import Polygon
 from triangular_pattern import generate_triangular_points
 from polygon_with_field_edge import reduced_polygon
-
+from coords_kml_tkinter import select_kml_file, extract_coordinates
+from coords_transformation import get_utm_zone, geo_to_utm
 
 class PlotApp(customtkinter.CTk):
     def __init__(self):
@@ -18,47 +19,70 @@ class PlotApp(customtkinter.CTk):
         self.title("Aussaatplanung")
         self.geometry("700x700")
 
-        #Eingabefelder
+        # Infotext Koordinaten
+        self.info_label = customtkinter.CTkLabel(self, text="")
+        self.info_label.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+        # Eingabefelder
         self.coords_label = customtkinter.CTkLabel(self, text="Koordinaten:")
-        self.coords_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-        self.coords_entry = customtkinter.CTkEntry(self)
-        self.coords_entry.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+        self.coords_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.coords_entry = customtkinter.CTkTextbox(self, width=100)
+        self.coords_entry.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+        self.coords_entry.bind("<FocusIn>", self.add_info_text_coords)
+        self.coords_entry.bind("<FocusOut>", self.remove_info_text)
 
         self.field_edge_label = customtkinter.CTkLabel(self, text="Feldrandbreite:")
-        self.field_edge_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.field_edge_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
         self.field_edge_entry = customtkinter.CTkEntry(self)
-        self.field_edge_entry.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+        self.field_edge_entry.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
 
         self.param2_label = customtkinter.CTkLabel(self, text="Pflanzenabstand:")
-        self.param2_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.param2_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
         self.param2_entry = customtkinter.CTkEntry(self)
-        self.param2_entry.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
+        self.param2_entry.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
 
         # Optionsliste
         self.option_plants_label = customtkinter.CTkLabel(self, text="Saatgut:")
-        self.option_plants_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        self.option_plants_label.grid(row=4, column=0, padx=10, pady=10, sticky="w")
         self.option_plants_entry = customtkinter.CTkOptionMenu(self, values=["", "Zuckerrübe", "Mais", "Getreide"], state="readonly")
-        self.option_plants_entry.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
+        self.option_plants_entry.grid(row=4, column=1, padx=10, pady=10, sticky="ew")
 
         # Button um Koordinaten der KML-File zu extrahieren
-        self.coords_kml = customtkinter.CTkButton(self, text="KML-Datei hochladen", command=self.extract_coordinates_kml)
-        self.coords_kml.grid(row=0, column=1, pady=10)
+        self.coords_kml = customtkinter.CTkButton(self, text="KML-Datei hochladen",
+                                                  command=self.extract_coordinates_kml)
+        self.coords_kml.grid(row=1, column=2, pady=10)
 
         # Button zum Berechnen und Anzeigen des Plots
-        self.calculate_button = customtkinter.CTkButton(self, text="Berechnen", command=self.calculate_and_show_plot)
-        self.calculate_button.grid(row=4, column=0, columnspan= 2, pady=10)
+        self.calculate_button = customtkinter.CTkButton(self, text="Berechnen", command= self.calculate_and_show_plot)
+        self.calculate_button.grid(row=6, column=0, columnspan= 2, pady=10)
+
+        # Tabs hinzufügen
+        self.tabview = customtkinter.CTkTabview(self)
+        self.tabview.grid(row=5, column=1, padx=20, pady=20)
+        self.tab1 = self.tabview.add("Tab 1")
+        self.tab2 = self.tabview.add("Tab 2")
+
+        # Widgets zu Tab 1 hinzufügen
+        self.label_tab1 = customtkinter.CTkLabel(master=self.tab1, text="Inhalt von Tab 1")
+        self.label_tab1.grid(row=5, column=1, padx=20, pady=10)
+
+        # Widgets zu Tab 2 hinzufügen
+        self.label_tab2 = customtkinter.CTkLabel(master=self.tab2, text="Inhalt von Tab 2")
+        self.label_tab2.grid(row=5, column=1, padx=20, pady=10)
 
         # Matplotlib-Figure für den Plot
         self.fig, self.ax = plt.subplots(figsize=(5, 4), dpi=100)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
-        self.canvas.get_tk_widget().grid(row=5, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+        self.canvas.get_tk_widget().grid(row=7, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
 
         # Platzhalter für den Plot
         self.plot_placeholder()
 
         # Zeilen- und Spaltenanpassung bei Lücken im Fenster
-        self.grid_rowconfigure(3, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        #self.grid_rowconfigure(1, weight=1)
+        #self.grid_rowconfigure(5, weight=0)  # weniger Gewicht für die Tabview-Zeile
+        #self.grid_columnconfigure(0, weight=1)
+        #self.grid_columnconfigure(2, weight=3)
 
     def calculate_and_show_plot(self):
         try:
@@ -103,13 +127,35 @@ class PlotApp(customtkinter.CTk):
             # Neuzeichnen der Matplotlib-Figur
             self.canvas.draw()
 
+        # Fehlermeldung für ungültige Eingaben
         except ValueError as error:
-            # Fehlerbehandlung für ungültige Eingaben
             messagebox.showerror("Fehler", f"Ungültige Eingabe: {str(error)}")
 
     def extract_coordinates_kml(self):
-        new_value = "42"
-        self.coords_entry.insert("2", new_value)
+        self.ax.clear()
+        selected_file = select_kml_file()
+        koordinaten = extract_coordinates(selected_file)
+        self.coords_entry.insert("1.0", text=koordinaten)
+        # Anhang der ersten Koordinate aus der KML-Datei aktuelle UTM-Zone ermitteln
+        utm_zone = get_utm_zone(koordinaten)
+        # Koordinaten in das UTM KGS projizieren, um in Metern weiterzurechnen
+        coords = geo_to_utm(koordinaten, utm_zone)
+        polygon = Polygon(coords)
+        x, y = polygon.exterior.xy
+        self.ax.fill(x, y, alpha=0.5, edgecolor='brown', facecolor='lightgreen')
+        self.ax.plot(*polygon.exterior.xy, color='black')
+        self.ax.set_aspect('equal', 'box')
+        self.ax.set_xlabel('Breitengrad')
+        self.ax.set_ylabel('Längengrad')
+        self.ax.set_title('Darstellung des Feldes')
+        # Neuzeichnen der Matplotlib-Figur
+        self.canvas.draw()
+
+    def add_info_text_coords(self, event):
+        self.info_label.configure(text="Info: Schreibe die Koordinaten der Feldpunkte in folgender Form auf: {Längengrad Breitengrad}{L2 B2}{L3 B3}...", text_color="gray")
+
+    def remove_info_text(self, event):
+        self.info_label.configure(text="")
 
     def plot_placeholder(self):
         # Platzhalter-Plot anzeigen
--- coords_kml_tkinter.py
+++ coords_kml_tkinter.py
@@ -16,21 +16,29 @@ def extract_coordinates(kml_file_path):
 
     coordinates = []
 
+    # Finde alle Koordinaten in den KML-Elementen
     for placemark in root.findall(".//{http://www.opengis.net/kml/2.2}Placemark"):
         coordinates_element = placemark.find(".//{http://www.opengis.net/kml/2.2}coordinates")
 
         if coordinates_element is not None:
             coordinates.extend(coordinates_element.text.strip().split())
 
-    return coordinates
+    # 3. Koordinate "0" entfernen, da nicht relevant
+    coordinates = [row[:-2] for row in coordinates]
 
+    # Umwandlung der Zeichenketten in numerische Werte (floats)
+    coordinates_num = [tuple(map(float, coord.split(','))) for coord in coordinates]
+
+    return coordinates_num
+
+if __name__ == "__main__":
 # Benutzer wählt die KML-Datei aus
-selected_file = select_kml_file()
+    selected_file = select_kml_file()
 
-# Überprüfe, ob eine Datei ausgewählt wurde
-if selected_file:
-    # Extrahiere Koordinaten und gebe sie aus
-    koordinaten = extract_coordinates(selected_file)
-    print(koordinaten)
-else:
-    print("Keine Datei ausgewählt.")
+    # Überprüfe, ob eine Datei ausgewählt wurde
+    if selected_file:
+        # Extrahiere Koordinaten und gebe sie aus
+        koordinaten = extract_coordinates(selected_file)
+        print(koordinaten)
+    else:
+        print("Keine Datei ausgewählt.")
